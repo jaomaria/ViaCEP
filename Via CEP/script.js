5df5a4980b76f9e33defb97880bdc36408d37705
Joao Pedro Santa Maria <165792797+jaomaria@users.noreply.github.com>
2024-10-04 08:29:58 -0300

    Add files via upload

--- Via CEP/script.js
+++ Via CEP/script.js
@@ -1,42 +1,45 @@
 function limpa_formulario_cep(){
-    document.getElementById("cep").value= "";
-    document.getElementById("rua").value= "";
-    document.getElementById("bairro").value= "";
-    document.getElementById("cidade").value= "";
-    document.getElementById("uf").value= "";
+    document.getElementById("cep").value = "";
+    document.getElementById("rua").value = "";
+    document.getElementById("bairro").value = "";
+    document.getElementById("cidade").value = "";
+    document.getElementById("uf").value = "";
 }
 
 function meu_callback(conteudo){
     if(!("erro" in conteudo)){
-        document.getElementById("rua").value= "conteudo.logradouro";
-        document.getElementById("bairro").value= "conteudo.bairro";
-        document.getElementById("cidade").value= "conteudo.cidade";
-        document.getElementById("uf").value= "conteudo.uf";
+        document.getElementById("rua").value = conteudo.logradouro;
+        document.getElementById("bairro").value = conteudo.bairro;
+        document.getElementById("cidade").value = conteudo.localidade;
+        document.getElementById("uf").value = conteudo.uf;
     
-}
-    else{
+} 
+else{
         limpa_formulario_cep();
-        alert("CEP não encontrado")
+        alert("CEP não encontrado");
     }
 }
 
-function pesuisacep(valor){
+function pesquisaCEP(valor){
     var cep = valor.replace(/\D/g,'');
-    if(cep != ""){
-        var validacep = /[0-9]{8}$/;
+
+    if (cep != "") {
+    var validacep = /[0-9]{8}$/;
+
         if(validacep.test(cep)){
-            document.getElementById("cep").value= "...";
-            document.getElementById("rua").value= "...";
-            document.getElementById("bairro").value= "...";
-            document.getElementById("cidade").value= "...";
-            document.getElementById("uf").value= "...";
+            
+            document.getElementById("rua").value = "...";
+            document.getElementById("bairro").value = "...";
+            document.getElementById("cidade").value = "...";
+            document.getElementById("uf").value = "...";
+
             var script = document.createElement('script');
-            script.src = 'https://viacep.com.br/ws'+ cep +'/json/?callback=meu_callback';
+            script.src = 'https://viacep.com.br/ws/'+ cep +'/json/?callback=meu_callback';
             document.body.appendChild(script)
         }
         else{
             limpa_formulario_cep();
-            alert("Insere um CEP valido ae pae")
+            alert("Insere um CEP valido ae pae");
         }
     }
     else{
